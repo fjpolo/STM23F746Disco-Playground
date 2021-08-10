c698f284e9ae161636a811438cb315f14b2b1481
Franco Polo <fjpolo@gmail.com>
2021-08-10 11:56:42 +0200

    Add 003Blinky.-

---
+++ 003Blinky/Release/BSP/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../BSP/bsp.cpp 
+
+OBJS += \
+./BSP/bsp.o 
+
+CPP_DEPS += \
+./BSP/bsp.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+BSP/%.o: ../BSP/%.cpp BSP/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m7 -std=gnu++14 -DUSE_HAL_DRIVER -DSTM32F746xx -c -I../Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I"D:/Franco/Dokumenten/STM32F7/STM32CubeIDE/workspace_1.7.0/003Blinky/BSP" -Os -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
