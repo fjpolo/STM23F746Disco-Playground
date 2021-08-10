c5d2f34fbbc36f5df05094cf1aac19973391b1fb
Franco Polo <fjpolo@gmail.com>
2021-08-10 12:09:23 +0200

    - Add 007BlinkyQP+QPcpp

---
+++ 007BlinkyQP/Release/Core/Startup/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32f746nghx.s 
+
+S_DEPS += \
+./Core/Startup/startup_stm32f746nghx.d 
+
+OBJS += \
+./Core/Startup/startup_stm32f746nghx.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m7 -c -I"D:/Programmen/qp/qpcpp/include" -I"D:/Programmen/qp/qpcpp/src" -I"D:/Programmen/qp/qpcpp/ports/arm-cm/qk/gnu" -I"D:/Programmen/qp/qpcpp/src/qk" -I"D:/Franco/Dokumenten/STM32F7/STM32CubeIDE/workspace_1.7.0/BSP" -I"D:/Franco/Dokumenten/STM32F7/STM32CubeIDE/workspace_1.7.0/007BlinkyQP/QPmodel" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
